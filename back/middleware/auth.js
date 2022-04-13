f310b44704e38b9d65a649ed86d454fa6fdd0fa4
Alicia <alicia.petitpas@gmail.com>
2022-04-13 14:08:08 +0200

    auth.js

---
+++ back/middleware/auth.js
@@ -0,0 +1,22 @@
+//Imp. Jsonwebtoken
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+//Vérification de la connexion de l'utilisateur grâce à un token
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.TOKEN_USER);
+        const userId = decodedToken.userId;
+        // req.auth = { userId };
+        if (req.body.userId && req.body.userId !== userId) {
+            res.status(403).json({ message: 'Unauthorized request' });
+        } else {
+            next();
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request')
+        });
+    }
+};
