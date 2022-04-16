5e1ef6904adb65f627f0fe272bef52c96e957ddb
Alicia <alicia.petitpas@gmail.com>
2022-04-16 19:37:05 +0200

    Storage des informations user avec axios

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,14 +1,28 @@
 import { createStore } from 'vuex'
 
+const axios = require('axios');
+
+const instance = axios.create({
+    baseURL: 'http://localhost:8080/'
+});
+
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {},
+    actions: {
+        //Récupère les données du formulaire d'inscription dans le store
+        createAccount: ({ commit }, userInfo) => {
+            commit;
+            instance.post('/createAccount', userInfo)
+                .then(function(response) {
+                    console.log(response)
+                        // commit('setStatus', 'created');
+                        // resolve(response);
+                })
+                .catch(function(error) {
+                    console.log(error)
+                        // commit('setStatus', 'error_create');
+                        // reject(error);
+                })
+        }
+    }
 })
