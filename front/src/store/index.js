c5c0333703174099a460cbfae922deea0057c249
Alicia <alicia.petitpas@gmail.com>
2022-04-18 11:34:48 +0200

    Modification baseURL

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -3,7 +3,7 @@ import { createStore } from 'vuex'
 const axios = require('axios');
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: '/api/'
 });
 
 export default createStore({
