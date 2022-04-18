31879cefd29b9a185930bde70711e06f0ffe4f44
Alicia <alicia.petitpas@gmail.com>
2022-04-18 18:02:08 +0200

    Routes MainPage, PostDetail et CreatePost

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,6 +3,9 @@ import HomePage from '../views/HomePage.vue'
 import HomeLogin from '../views/HomeLogin.vue'
 import HomeRegister from '../views/HomeRegister.vue'
 import UserProfile from '../views/UserProfile.vue'
+import MainPage from '../views/MainPage.vue'
+import PostDetail from '../views/PostDetail.vue'
+import CreatePost from '../views/CreatePost.vue'
 
 const routes = [{
         path: '/',
@@ -23,6 +26,21 @@ const routes = [{
         path: '/userProfile',
         name: 'userProfile',
         component: UserProfile
+    },
+    {
+        path: '/mainPage',
+        name: 'mainPage',
+        component: MainPage
+    },
+    {
+        path: '/postDetail',
+        name: 'postDetail',
+        component: PostDetail
+    },
+    {
+        path: '/createPost',
+        came: 'createPost',
+        component: CreatePost
     }
 ]
 
