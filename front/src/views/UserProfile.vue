fa3b1bc746ec2718f49c4927d7ef776aa9ba1e26
Alicia <alicia.petitpas@gmail.com>
2022-04-18 15:17:45 +0200

    Création vue UserProfile

---
+++ front/src/views/UserProfile.vue
@@ -0,0 +1,27 @@
+<template>
+    
+<HeaderPage/>
+
+<main class="main">
+
+    <div class="profile">
+        <!-- User Info -->
+        <p>this is a test</p>
+    </div>
+
+</main>
+
+</template>
+
+<script>
+
+import HeaderPage from '../components/HeaderPage.vue'
+
+export default {
+    name: 'UserProfile',
+    components: {
+        HeaderPage,
+    }
+}
+
+</script>
