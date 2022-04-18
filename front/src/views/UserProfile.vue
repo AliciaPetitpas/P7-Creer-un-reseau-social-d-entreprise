12884c7bf7e02e2e834bcabc7f717b0f4b4438ab
Alicia <alicia.petitpas@gmail.com>
2022-04-18 15:43:41 +0200

    Style userProfile

--- front/src/views/UserProfile.vue
+++ front/src/views/UserProfile.vue
@@ -6,7 +6,17 @@
 
     <div class="profile">
         <!-- User Info -->
-        <p>this is a test</p>
+        <div class="user">
+            <div class="user-profile-picture">
+                <img src="" alt="user-picture">
+            </div>
+            <div class="user-info">
+                <p class="first_name">First name</p>
+                <p class="last_name">Last name</p>
+                <p class="email">Email</p>
+            </div>
+            <button class="modify-user-info">Modifier informations</button>
+        </div>
     </div>
 
 </main>
@@ -25,3 +35,45 @@ export default {
 }
 
 </script>
+
+<style scoped>
+
+div {
+    display: flex;
+    flex-direction: column;
+    margin: 10px auto 10px auto;
+}
+
+.profile {
+    background-color: #fdd7d7 ;
+    border: 1px solid black;
+    border-radius: 30px;
+}
+
+.user {
+    width: 80%;
+}
+
+.user-info {
+    flex-direction: row;
+}
+
+.user-profile-picture {
+    position: absolute;
+    width: 150px;
+    height: 150px;
+    border: 1px solid black;
+    border-radius: 150px;
+}
+
+button {
+    margin: 10px auto 10px auto;
+    width: 30%;
+    padding: 30px;
+    border-style: none;
+    background-color: black;
+    color: white;
+    font-weight: bold;
+}
+
+</style>
