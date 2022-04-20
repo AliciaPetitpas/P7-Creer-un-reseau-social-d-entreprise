555e5a1df372cc6aa8cd6c1c723d62e4acbf3eb3
Alicia <alicia.petitpas@gmail.com>
2022-04-20 12:13:47 +0200

    Method connexion application

--- front/src/views/HomeRegister.vue
+++ front/src/views/HomeRegister.vue
@@ -5,11 +5,26 @@
         <!-- Formulaire inscription -->
 
         <div class="form">
-            <input v-model="first_name" type="text" class="input-form" placeholder="Nom"/>
-            <input v-model="last_name" type="text" class="input-form" placeholder="Prénom"/>
-            <input v-model="email" type="email" class="input-form" placeholder="Adresse mail"/>
-            <input v-model="password" type="password" class="input-form" placeholder="Mot de passe"/>
-            <button @click="createAccount()" class="button" :class="{'button--disabled' : !validateForm}">Créer mon compte</button>
+            <input v-model="state.input.last_name" type="text" class="input-form" placeholder="Nom"/>
+            <span v-if="v$.input.last_name.$error" class="error">
+                {{ v$.input.last_name.$errors[0].$message }}
+            </span>
+            <input v-model="state.input.first_name" type="text" class="input-form" placeholder="Prénom"/>
+            <span v-if="v$.input.first_name.$error" class="error">
+                {{ v$.input.first_name.$errors[0].$message }}
+            </span>
+            <input v-model="state.input.email" type="email" class="input-form" placeholder="Adresse mail"/>
+            <span v-if="v$.input.email.$error" class="error">
+                {{ v$.input.email.$errors[0].$message }}
+            </span>
+            <input v-model="state.input.password" type="password" class="input-form" placeholder="Mot de passe"/>
+            <span v-if="v$.input.password.$error" class="error">
+                {{ v$.input.password.$errors[0].$message }}
+            </span>
+            <button @click="createAccount()" class="button">Créer mon compte</button>
+            <span class="error">
+                {{ error }}
+            </span>
         </div>
 </div>
 
@@ -17,43 +32,103 @@
 
 <script>
 
-import { mapState } from 'vuex'
-
 import HeaderPage from '../components/HeaderPage.vue'
+import useValidate from "@vuelidate/core";
+import { required, email, minLength, helpers } from "@vuelidate/validators";
+import { reactive, computed } from "vue";
 
 export default {
     name: 'HomeRegister',
     components: {
         HeaderPage,
     },
+        setup() {
+        const state = reactive({
+            input: {
+                last_name: "",
+                first_name: "",
+                email: "",
+                password: "",
+            }
+        });
+        const rules = computed(() => {
+            return {
+                input: {
+                    last_name: {
+                        required: helpers.withMessage(
+                            "Veuillez renseigner ce champ !",
+                            required
+                        )
+                    },
+                    first_name: {
+                        required: helpers.withMessage(
+                            "Veuillez renseigner ce champ !",
+                            required
+                        )
+                    },
+                    email: {
+                        required: helpers.withMessage(
+                            "Veuillez renseigner ce champ !",
+                            required
+                        ),
+                        email: helpers.withMessage(
+                            "Veuillez saisir une adresse mail valide !",
+                            email
+                        ),
+                    },
+                    password: {
+                        required: helpers.withMessage(
+                            "Veuillez renseigner ce champ !",
+                            required
+                        ),
+                        minLength: helpers.withMessage(
+                            "Le mot de passe doit comporter 4 caractères minimum !",
+                            minLength(4)
+                        ),
+                    },
+                },
+            };
+        });
+        const v$ = useValidate(rules, state);
+        return {
+            state,
+            v$,
+        };
+    },
     data: function () {
         return {
-            mode: 'register',
-            first_name: '',
-            last_name: '',
-            email: '',
-            password: '',
+            error: "",
         }
-    },
-    computed: {
-        validateForm: function() {
-            if (this.first_name != '' && this.last_name != '' && this.email != '' && this.password != '') {
-                return true;
-            } else {
-                return false;
+     },
+    methods: {
+        login: function () {
+            this.v$.$validate();
+            if (!this.v$.$error) {
+                const self = this;
+                this.$store.dispatch('loginAccount', {
+                    email: this.state.input.email,
+                    password: this.state.input.password
+                }).then(function () {
+                    self.$router.push('/userProfile');
+                }, function (error) {
+                    self.error = error.response.data.error;
+                })
             }
         },
-        ...mapState(['status'])
-    },
-    methods: {
         createAccount: function () {
-            // console.log(this.first_name, this.last_name, this.email, this.password);
-            this.$store.dispatch('createAccount', {
-                first_name: this.first_name,
-                last_name: this.last_name,
-                email: this.email,
-                password: this.password
-            })
+            // console.log(this.state.input.last_name, this.state.input.first_name, this.state.input.email, this.state.input.password);
+            const self = this;
+            this.$store.dispatch('createAccount', {           
+                last_name: this.state.input.last_name,
+                first_name: this.state.input.first_name,
+                email: this.state.input.email,
+                password: this.state.input.password
+            }).then(function() {
+                self.login();
+            },function (error) {
+                self.error = error.response.data.error;
+            }
+            )
         }
     }
 }
