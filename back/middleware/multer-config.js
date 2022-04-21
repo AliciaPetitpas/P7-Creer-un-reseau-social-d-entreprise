bbd1dfc12f55ee2c1de929efa0c454cc512ca51f
Alicia <alicia.petitpas@gmail.com>
2022-04-21 16:31:34 +0200

    rectification multer

--- back/middleware/multer-config.js
+++ back/middleware/multer-config.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const path = require('path');
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -9,13 +10,15 @@ const MIME_TYPES = {
 
 const storage = multer.diskStorage({
     destination: (res, file, callback) => {
-        callback(null, 'images');
+        if (file.fieldname === "image_post") callback(null, './images/posts/');
+        else if (file.fieldname === "image_profil") callback(null, 'images/profils/');
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
+        callback(null, Date.now() + path.extname(file.originalname));
+        // const name = file.originalname.split(' ').join('_');
+        // const extension = MIME_TYPES[file.mimetype];
+        // callback(null, name + Date.now() + '.' + extension);
     }
 });
 
-module.exports = multer({ storage: storage }).single('image');
+module.exports = multer({ storage: storage });
