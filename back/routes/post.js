f2a74689d6f8a5f0182a898e5788d2652ef0e087
Alicia <alicia.petitpas@gmail.com>
2022-05-02 10:36:26 +0200

    Route pour createPost

--- back/routes/post.js
+++ back/routes/post.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const db = require("../models");
 
-// const multer = require('../middleware/multer-config');
-// const auth = require('../middleware/auth');
-// const postCtrl = require('../controllers/post')
+//const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth');
+const postCtrl = require('../controllers/post')
 
 // Routes publications
-//router.post('/post', multer, postCtrl.createPost);
+router.post('/createPost', auth, postCtrl.createPost);
 //router.delete('/post/:id', postCtrl.deletePost);
 //router.get('/getPost', postCtrl.getAllPosts);
 
