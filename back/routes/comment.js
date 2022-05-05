053d510bb62f8014e8252e8c4c7333c8ead2e9f6
Alicia <alicia.petitpas@gmail.com>
2022-05-05 16:20:34 +0200

    Modification routes comment, post & user

--- back/routes/comment.js
+++ back/routes/comment.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-router.post('/new', (req, res) => {
-    db.Post.create({
-        content: req.body.content,
-    }).then(newPost => res.send(newPost));
-});
+// router.post('/new', (req, res) => {
+//     db.Post.create({
+//         content: req.body.content,
+//     }).then(newPost => res.send(newPost));
+// });
 
-router.get('/find/:id', (req, res) => {
-    db.Post.findAll({
-        where: { PostId: req.params.id },
-        // include: [db.User]
-    }).then(post => res.send(post));
-})
+// router.get('/find/:id', (req, res) => {
+//     db.Post.findAll({
+//         where: { PostId: req.params.id },
+//         // include: [db.User]
+//     }).then(post => res.send(post));
+// })
 
 module.exports = router;
--- back/routes/post.js
+++ back/routes/post.js
@@ -11,19 +11,4 @@ router.post('/createPost', auth, postCtrl.createPost);
 //router.delete('/post/:id', postCtrl.deletePost);
 //router.get('/getPost', postCtrl.getAllPosts);
 
-router.post('/new', (req, res) => {
-    db.Post.create({
-        title: req.body.title,
-        content: req.body.content,
-        // imageUrl: req.body.imageUrl,
-    }).then(newPost => res.send(newPost));
-});
-
-router.get('/find/:id', (req, res) => {
-    db.Post.findAll({
-        where: { UserId: req.params.id },
-        include: [db.User]
-    }).then(post => res.send(post));
-})
-
 module.exports = router;
