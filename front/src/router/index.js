25c7be5fb0ddffebdbc6b564e8b3dc1b979f1f14
Alicia <alicia.petitpas@gmail.com>
2022-05-12 17:13:17 +0200

    Ajout de la vue modification post

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -7,6 +7,7 @@ import MainPage from '../views/MainPage.vue'
 import PostDetail from '../views/PostDetail.vue'
 import CreatePost from '../views/CreatePost.vue'
 import ModifyPassword from '../views/ModifyPassword.vue'
+import ModifyPost from '../views/ModifyPost.vue'
 
 const routes = [{
         path: '/',
@@ -47,7 +48,12 @@ const routes = [{
         path: '/modifyPassword',
         name: 'modifyPassword',
         component: ModifyPassword
-    }
+    },
+    {
+        path: '/modifyPost',
+        name: 'modifyPost',
+        component: ModifyPost
+    },
 ]
 
 const router = createRouter({
---
+++ front/src/views/ModifyPost.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <MenuPage/>
+        <main id="main" class="main">
+            <div class="modify-post">
+                <p>This is where we update the post</p>
+                <!-- <p>{{ posts }}</p> -->
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+
+import MenuPage from '../components/MenuPage.vue'
+import { mapState } from 'vuex'
+
+export default {
+    name: 'modifyPost',
+    components: {
+        MenuPage,
+    },
+    computed: {
+        ...mapState({
+            user:'user',
+            userInfo: 'userInfo',
+            // posts: 'posts',
+        }),
+    },
+}
+</script>
